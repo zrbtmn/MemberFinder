f0f82115a13afc0a7892ba9a741aa730db951e20
hotline4beach <zrbtmn@gmail.com>
2020-02-18 14:41:43 +0300

    Release v1.0

---
+++ Member.java
@@ -0,0 +1,17 @@
+public class Member {
+    private final String name;
+    private final Integer age;
+
+    public Member(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
